778fcd084277a4528b517de488571c0c2ad14cc3
heliumface770 <heliumface770@gmail.com>
2019-09-28 19:45:19 -0600

    event listeners fixed

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ var charLength = false;
 var boxChecked = false;
 var numHolder = 0;
 var password = "";
+var generateBtn = document.querySelector("#generateButton");
+var copyBtn = document.querySelector("#copyButton");
+var numEl = document.querySelector("#inputNum");
+var symChkEl = document.getElementById("symChk");
+var numChkEl = document.getElementById("numChk");
+var lowChkEl = document.getElementById("lowChk");
+var capChkEl = document.getElementById("capChk");
 
 function numUpdate() {
   var x = document.getElementById("inputNum").value;
@@ -80,11 +87,22 @@ function randomCharacters(letters) {
   return result;
 }
 
-function copyFunction() {
+function copy() {
   var copyText = document.getElementById("password");
 
   copyText.select();
-  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  copyText.setSelectionRange(0, 99999);
   document.execCommand("copy");
   alert("Password Copied To Clipboard");
 }
+
+generateBtn.addEventListener("click", generate);
+
+copyBtn.addEventListener("click", copy);
+
+numEl.addEventListener("keyup", numUpdate);
+
+symChkEl.addEventListener("change", boxCheckUpdate);
+numChkEl.addEventListener("change", boxCheckUpdate);
+lowChkEl.addEventListener("change", boxCheckUpdate);
+capChkEl.addEventListener("change", boxCheckUpdate);
